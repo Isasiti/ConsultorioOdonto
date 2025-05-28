1ce337bf49fbf2f4806f401c629035eed4e93043
isasiti <isasiti06@gmail.com>
2025-05-28 00:15:08 -0500

    backup

--- Usuario.py
+++
@@ -1,8 +0,0 @@
-class Usuario:
-    _USUARIO_ESPERADO = "lacabramosquera"
-    _CONTRASEÑA_ESPERADA = "NOPUEDOMAS"
-
-    def verificar_credenciales(self, usuario: str, contraseña: str) -> bool:
-        return usuario == self._USUARIO_ESPERADO and contraseña == self._CONTRASEÑA_ESPERADA
-
-     
